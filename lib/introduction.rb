38e99d9a537647cf03241dff541b112fb6217381
John Fuquene <jfuquene@email.wm.edu>
2020-11-05 18:59:30 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,5 @@
 def introduction(name)
-  puts "Hi, my name is #{name}"
-  name = "Josh"
+  puts "Hi, my name is #{name}."
 end 
   
   
@@ -8,7 +7,9 @@ def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
   
-
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
